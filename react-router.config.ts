ca8fc5d5127f131126fd993d637f14c53a97174d
rohit <rohitsoni259@gmail.com>
2025-09-19 17:51:25 +0530

    final changes

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,7 +4,7 @@ export default {
   // Disable SSR to make it a static SPA for GitHub Pages
   ssr: false,
   // Set the basename to match your GitHub Pages URL
-  basename: "/portfolio",
+  // basename: "/portfolio",
   // base: "/portfolio",
   // Config options...
   async prerender() {
