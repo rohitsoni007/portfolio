195557d02480b9dafbafe136e935e509b5e9c63b
rohit <rohitsoni259@gmail.com>
2025-09-19 17:38:30 +0530

    base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  // base: "/portfolio/",
+  base: "/portfolio/",
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
